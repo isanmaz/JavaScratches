deef0e437cd5ca95e0bcc46eda722660ae0dea97
isanmaz <aysan.mazlumi@gmail.com>
2023-03-03 15:58:26 +0100

    solution to whenBuyStock

---
+++ src/codingproblems/BestTimeBuySellStock.java
@@ -0,0 +1,40 @@
+package codingproblems;
+
+public class BestTimeBuySellStock {
+/*
+ * Input: prices = [7,1,5,3,6,4] max = 0;
+ * starting from 7
+ * 
+ * */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+    public static int maxProfit(int[] prices) {
+	int totalMax = 0;
+	int left=0;
+	int right=1;
+	if (prices.length<=1) {
+	    return prices[0];
+	}
+	
+	for (int i = 0; i < prices.length; i++) {
+	    if (prices[left]<prices[right] && totalMax<=prices[right]-prices[left]) {
+		totalMax = prices[right]-prices[left];
+	    }
+	    else if (prices[left]>prices[right]) {
+		left = right;
+	    }
+	    if (right<prices.length-1) {
+		right++;
+	    }
+	    
+	}
+	
+	
+	
+	return totalMax;
+
+    }
+
+}
