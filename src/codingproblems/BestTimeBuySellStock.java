0e6f82ee6b06693d674f67f6393f45403920292f
isanmaz <aysan.mazlumi@gmail.com>
2023-03-03 16:08:02 +0100

    changes

--- src/codingproblems/BestTimeBuySellStock.java
+++ src/codingproblems/BestTimeBuySellStock.java
@@ -7,7 +7,9 @@ public class BestTimeBuySellStock {
  * 
  * */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	int[] input = {2,7,11,15};
+	int profit = maxProfit(input);
+System.out.println(profit);
 
     }
     public static int maxProfit(int[] prices) {
