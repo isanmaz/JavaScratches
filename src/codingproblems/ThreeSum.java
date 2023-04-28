6122e27de18c9c0518a75cbb56e59402c2733529
isanmaz <aysan.mazlumi@gmail.com>
2023-04-28 09:20:17 +0200

    back to problem solving with 3 sum

--- src/codingproblems/ThreeSum.java
+++ src/codingproblems/ThreeSum.java
@@ -1,47 +1,68 @@
-//package codingproblems;
-//
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//import com.sun.org.apache.regexp.internal.recompile;
-//
-//import javafx.util.Pair;
-//
-//public class ThreeSum {
-//
-//    public static void main(String[] args) {
-//	// TODO Auto-generated method stub
-//
-//    }
-//    public static List<List<Integer>> threeSum(int[] nums) {
-//	List<List<Integer>> result = new ArrayList<List<Integer>>();
-//	if (nums.length<3) {
-//	    return Collections.emptyList();
-//	}
-//	List<Integer> subs= new ArrayList<Integer>();
-//	int l=0;
-//	int r=1;
-//	Map< Integer,Pair<Integer, Integer>> pairVsSub= new HashMap<Integer,Pair<Integer,Integer>>();
-//	for (int i = 0; i < nums.length; i++) {
-//	    int left= i+1;
-//	    int right=i+2;
-//	    int temp= nums[left]+nums[right];
-//	    if (subs.contains(temp*(-1))) {
-//		List<Integer> subArray= new ArrayList<Integer>();
-//		subArray.add(l);
-//		subArray.add(r);
-//		Pair<Integer, Integer> leftRight = new Pair<Integer, Integer>(l, r);
-//		subArray.add(pairVsSub.get(leftRight));
-//	    }else {
-//		Pair<Integer, Integer> leftRight = new Pair<Integer, Integer>(l, r);
-//		pairVsSub.put(leftRight, temp*(-1));
-//	    }
-//	}
-//	
-//	return null;
-//    }
-//
-//}
+package codingproblems;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry.Entry;
+
+class ThreeSum{
+    
+    public static void main(String arg[]) {
+	
+	int[] input = {0,0,0,0} ;
+	System.out.println(threeSum(input));
+	
+	
+	
+    }//-4,-1,-1,0,1,2
+    public static List<List<Integer>> threeSum(int[] nums) {
+	//int[] nums = Arrays.stream(numbers).sorted().distinct().toArray();
+	Arrays.sort(nums);
+	Set<List<Integer>> result = new HashSet();
+	for (int i = 0; i < nums.length; i++) {
+	    int left=i+1;
+	    int right = nums.length-1;
+	    if (left==i) {
+		left++;
+	    }
+	    
+	    while (left<right) {
+		 
+	    if (nums[i] + nums[left]+nums[right]==0) {
+		List<Integer> sult = new ArrayList<Integer>();
+		sult.add(nums[i]);
+		sult.add(nums[left]);
+		sult.add(nums[right]);
+		result.add(sult);
+		while (nums[left]!=nums[left-1]&& left<right) {
+		    
+		    left++;
+		    
+		}
+}
+	    
+	    else if (nums[i] + nums[left]+nums[right]>0) {
+		right--;
+	    }else {
+		left++;
+	    }
+	    }
+	    
+	    
+    
+	}
+        
+	return new ArrayList<List<Integer>>(result);
+    }
+
+	
+    
+    
+    
+}
